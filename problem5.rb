73346270ca6e0f9b420c897e8fc85310734efc53
Sora Harakami <sora134@gmail.com>
2010-09-08 19:32:38 +0900

    norrrrr

--- problem5.rb
+++ problem5.rb
@@ -1,13 +1,11 @@
 # Problem 5
 
-i = 50000000
+i = 0
 loop do
-  Thread.new do
-    p i if i % 1000000 == 0
-    if (2..20).all? { |j| i % j == 0 }
-      p i
-      exit
-    end
+  p i if i % 1000000 == 0
+  if (2..20).all? { |j| i % j == 0 }
+    p i
+    exit
   end
   i += 1
 end
